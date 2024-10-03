352978fd6685e99564f1474aa75acde3e31a15a9
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-10-04 00:47:43 +0530

    foods

--- 18-fragments/fragments-01/src/App.jsx
+++ 18-fragments/fragments-01/src/App.jsx
@@ -3,16 +3,16 @@ import React from "react";
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <h1>Healthy Foods</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        <li className="list-group-item">Dal</li>
+        <li className="list-group-item">Roti</li>
+        <li className="list-group-item">Salad</li>
+        <li className="list-group-item">Green Vegetable</li>
+        <li className="list-group-item">Milk</li>
       </ul>
-    </React.Fragment>
+    </>
   );
 }
 
