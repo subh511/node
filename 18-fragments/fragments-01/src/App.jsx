bc623e1355057ba5c98c239c023584eb6e6b19c6
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-10-04 01:20:29 +0530

    adding ternary operator

--- 18-fragments/fragments-01/src/App.jsx
+++ 18-fragments/fragments-01/src/App.jsx
@@ -1,26 +1,24 @@
-import React from "react";
-
-
 function App() {
+  // let foodItems = [];
+  let foodItems = ["Dal", "Roti", "Salad", "GreenVegetable", "Milk", "Ghee"];
 
-  let foodItems = [
-    'Dal',
-    'Roti',
-    'Salad',
-    'GreenVegetable',
-    'Milk',
-    'Ghee'
-  ]
+  // if (!foodItems.length) {
+  //   return <h2>I am Hungry</h2>;
+  // }
   return (
     <>
       <h1>Healthy Foods</h1>
-      <ul className="list-group">
-      {
-        foodItems.map((food)=>
-          <li className="list-group-item">{food}</li>
-       )
-      }
-      </ul>
+      {!foodItems.length ? (
+        <h2>I am Hungry</h2>
+      ) : (
+        <ul className="list-group">
+          {foodItems.map((food) => (
+            <li className="list-group-item" key={food}>
+              {food}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
