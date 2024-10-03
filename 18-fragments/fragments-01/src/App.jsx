efe7c415dd8e1fa20ce923d52708e7a8e41cdc68
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-10-04 00:57:15 +0530

    map

--- 18-fragments/fragments-01/src/App.jsx
+++ 18-fragments/fragments-01/src/App.jsx
@@ -2,15 +2,24 @@ import React from "react";
 
 
 function App() {
+
+  let foodItems = [
+    'Dal',
+    'Roti',
+    'Salad',
+    'GreenVegetable',
+    'Milk',
+    'Ghee'
+  ]
   return (
     <>
       <h1>Healthy Foods</h1>
       <ul className="list-group">
-        <li className="list-group-item">Dal</li>
-        <li className="list-group-item">Roti</li>
-        <li className="list-group-item">Salad</li>
-        <li className="list-group-item">Green Vegetable</li>
-        <li className="list-group-item">Milk</li>
+      {
+        foodItems.map((food)=>
+          <li className="list-group-item">{food}</li>
+       )
+      }
       </ul>
     </>
   );
