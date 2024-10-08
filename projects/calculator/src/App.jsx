402691673410d5de00d4d473cba170e8777f49af
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-10-08 23:18:14 +0530

    added all items

--- projects/calculator/src/App.jsx
+++ projects/calculator/src/App.jsx
@@ -1,18 +1,12 @@
 import styles from "./App.module.css";
-
+import Display from "./components/Display";
+import ButtonContainer from "./components/ButtonContainer";
 function App() {
 
   return (
     <div className={styles.calculator}>
-    <input type="text" className={styles.display}/>
-    <div className={styles.button}>
-    <button className={styles.buttons}>1</button>
-    <button className={styles.buttons}>2</button>
-    <button className={styles.buttons}>3</button>
-    <button className={styles.buttons}>4</button>
-    <button className={styles.buttons}>5</button>
-    <button className={styles.buttons}>6</button>
-    </div>
+    <Display/>
+    <ButtonContainer/>
     </div>
   )
 }
