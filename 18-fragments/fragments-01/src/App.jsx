a60fd95bfea74b59d955a820036d34608cc40b4a
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-10-09 12:39:22 +0530

    set state

--- 18-fragments/fragments-01/src/App.jsx
+++ 18-fragments/fragments-01/src/App.jsx
@@ -3,15 +3,27 @@ import ErrorMessage from "./components/ErrorMsg";
 import FoodInput from "./components/FoodInput";
 import FoodItems from "./components/FoodItems";
 
+import {useState} from "react";
+
 function App() {
   // let foodItems = [];
   let foodItems = ["Sabzi", "Roti", "Salad", "GreenVegetable", "Milk", "Ghee"];
+
+ // let textToShow = "Food Item Entered by User";
+
+  let [textToShow,setTextState] = useState("Food Item Entered by User");
+
+  const handleOnChange = (event) =>{
+    console.log(event.target.value)
+    setTextState(event.target.value);
+  }
 
   return (
     <Container>
       <h1>Healthy Foods</h1>
       <ErrorMessage foodItems={foodItems}/>
-      <FoodInput/>
+      <FoodInput handleOnChange={handleOnChange}/>
+      <p>{textToShow}</p>
       <FoodItems foodItems={foodItems}/>
     </Container>
   );
